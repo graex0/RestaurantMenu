9aa77775feea378145742b905138ab7d35511803
graex0 <graex0@gmail.com>
2021-09-30 19:15:46 -0400

    * .gitignore: Initial commit.
    * RestaurantMenu.sln:
    * Program.cs:
    * RestaurantMenu.csproj:

---
+++ RestaurantMenu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RestaurantMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public class MenuItem
+        {
+            string itemName;
+            int price;
+            string description;
+            string category;
+        }
+    }
+}
